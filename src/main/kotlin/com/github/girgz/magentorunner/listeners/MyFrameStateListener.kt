f82d59ca6cfadd5694698520a44b27b261cbb252
GitHub Action <action@github.com>
2023-06-03 13:35:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/girgz/magentorunner/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/girgz/magentorunner/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.girgz.magentorunner.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
